105d5b2eec9baeb8aa87c53c4c989affddcd2f90
yoshikouki <yoshikouki@gmail.com>
2023-12-02 17:58:46 +0900

    Create article editor component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,9 @@
+import { ArticleEditor } from "@/components/article-editor";
+
 export default function HomePage() {
   return (
     <main className="p-4">
+      <ArticleEditor />
     </main>
   );
 }
